bd34bc60cdd3183e5d3420c9810ea1a428fa8b02
Qiu Fen <2974327782@qq.com>
2025-04-24 22:47:19 +0800

    2.1

---
+++ 18996 求正弦函数值/源.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+#define PI 3.1415926535897932l
+
+// 泰勒级数展开
+// 根据 x 的大小，返回计算 sin(x) 所需的泰勒展开项数 N
+// x越小，泰勒展开收敛越快，需要的项数越少；反之需要更多项来保证精度
+
+int GetSinItemNumber(double x) {
+    int N = 14;
+    if (x < 0)x = -x;
+    if (x <= PI / 8) N = 6;
+    else if (x <= PI / 4) N = 8;
+    else if (x <= PI / 2) N = 10;
+    else if (x <= 3 * PI / 4) N = 12;
+    return N;
+}
+
+double SINTNV(double x) {
+
+    double y = 1.0;
+    int N = GetSinItemNumber(x);
+    int K = N * 2;
+    double x_squared = x * x;
+    while (K > 0) {
+        y = 1.0 - y * x_squared / (K * (K + 1));
+        K -= 2; // 每次处理两项
+    }
+    return y * x;
+}
+
+
+signed main() {
+	std::cout << std::fixed << std::setprecision(14);
+	int m;
+	std::cin >> m;
+	double x;
+	for (int i = 0; i < m; i++) {
+        std::cin >> x;
+        std::cout << SINTNV(x) << std::endl;
+	}
+}
